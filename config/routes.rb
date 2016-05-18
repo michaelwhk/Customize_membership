b42ad05d30f8cdd4d66471b66fc528c7a481f9e1
Michael <wanghk@Pro-M.local>
2016-05-18 17:59:31 +1000

    Fix problem, user could sign up and cancel

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   get 'products/:id', to: 'products#show', :as => :products
   devise_for :users, :controllers => { :registrations => 'registrations'}
+  devise_scope :user do
+    put 'change_plan', :to => 'registrations#change_plan'
+  end
   resources :users
 end
